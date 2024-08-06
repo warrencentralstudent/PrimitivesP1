5cad7bebd6d264563f5ba662d3364a5907ecc987
Davian Affognon <affogdav000@warren.k12.in.us>
2024-08-06 11:32:13 -0400

    update

--- src/Primitives.java
+++ src/Primitives.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Primitives {
+public class Primitives {
+    public static void main(String[] args) {
+        int myInt = 7; // 32 bits: 2^32 different values
+        System.out.println(myInt);
+        short myShort = 8; // 16 bits 2^16 different values
+        System.out.println(myShort);
+        byte myByte =8; // 8 bits 2^8 different values
+        System.out.println(myByte);
+        long myLong = 23049827372384L; // 64 bits 2^64 different values
+        System.out.println(myLong);
+
+        boolean myBoolean = true;
+        boolean myBoolean2 = false;
+
+        String myString = "Hello World";
+
+        char myChar = 'a';
+        System.out.println(myChar);
+
+        Integer myInteger = 7; // 32 bits: 2^32 different values
+        System.out.println(myInt);
+        Short myShort2 = 8; // 16 bits 2^16 different values
+        System.out.println(myShort);
+        Byte myByte2 =8; // 8 bits 2^8 different values
+        System.out.println(myByte);
+        Long myLong2 = 23049827372384L; // 64 bits 2^64 different values
+        System.out.println(myLong);
+
+        System.out.println(Character.MIN_VALUE);
+        System.out.println(Character.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+
+        int myOtherInt3 = 2_145_178;
+        System.out.println(Integer.MAX_VALUE + 1);
+
+
+    }
+
+
 }
